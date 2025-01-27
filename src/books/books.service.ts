1a1e52189353966f301ce638b3476857a11e08c9
Pawan Mishra <pawanm9816@gmail.com>
2025-01-27 13:29:11 +0545

    added books crud

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Book } from './entities/book.entity';
+
+
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  constructor(private readonly prismaService: PrismaService){}
+
+   async create(createBookDto: CreateBookDto) {
+
+    const book = await this.prismaService.books.findFirst({
+      where: {
+        name: createBookDto.title,
+      },
+    });
+
+   if (book) {
+     throw new BadRequestException (`Books ${createBookDto.title} has been alreadyyyyy taken`);
+   }
+
+   return this.prismaService.books.create({ data: createBookDto});
+    //return 'This action adds a new book';
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.prismaService.books.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.getBooksById(id);
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+   async update(id: number, updateBookDto: UpdateBookDto) {
+   await this.getBooksById(id);
+
+   const book = await this.prismaService.books.findFirst({
+    where: {
+      title: updateBookDto.title,
+    },
+   });
+
+   if(book && book.id !== id) {
+    throw new BadRequestException(`book with ${updateBookDto.title} has been alreadyyyy taken`);
+   }
+    return this.prismaService.books.update({
+      where: { id} ,
+      data: updateBookDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+   async remove(id: number) {
+    await this.getBooksById(id);
+    return this.prismaService.books.delete({
+      where: { id}
+    });
+  }
+
+  private async getBooksById(id: number) {
+    const role = await this.prismaService.books.findFirst({ where: { id } });
+
+    if (!role) {
+      throw new NotFoundException(`Books with id ${id} does not exist`);
+    }
+
+    return Book;
   }
 }
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBookDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, isString, IsString } from "class-validator";
+
+export class CreateBookDto {
+
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsOptional()
+    @IsString()
+    author : string;
+
+   @IsString()
+   @IsOptional()
+   price: number;
+
+   @IsString()
+   @IsBoolean()
+   @IsOptional()
+   available: Boolean;
+
+}
